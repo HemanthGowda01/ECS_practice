5dd9db5eee7f17fe92f13fea4dee2d43c96b491f
Hemanth_Gowda_01 <tinycamvlogs@gmail.com>
2024-01-15 16:04:08 +0530

    new files

---
+++ ECR/ecr.tf
@@ -0,0 +1,4 @@
+resource "aws_ecr_repository" "demo_ecr_app_repo" {
+    name = var.ecr_repo_name
+  
+}
---
+++ ECR/outputs.tf
@@ -0,0 +1,4 @@
+output "repository_url" {
+    value = aws_ecr_repository.demo_ecr_app_repo.repository_url
+  
+}
